3175fadaa098551290e7b39ac1ae9da62622c6d6
ravp90 <r.parekh90@gmail.com>
2022-12-20 16:55:12 +0000

    adding setup files for k8s

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a8b6a24a0e757460fa24c1fe07ffad8c-1879923826.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
